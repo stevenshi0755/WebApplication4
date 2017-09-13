c90b98014adbb8a88d4fecc448d8f238b4dd1a24
义庆 石 <szsyq123@163.com>
2017-09-13 16:56:25 +0800

    添加项目文件。

---
+++ WebApplication4/Controllers/EmployeeBusinessLayer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class EmployeeBusinessLayer
+    {
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            //{
+            //    new Employee {FirstName="Johnson",LastName="fernandes",Salary=14000 },
+            //    new Employee {FirstName="Michael",LastName="Jackson",Salary=16000 },
+            //    new Employee {FirstName="Robert",LastName="Pattinson",Salary=20000 },
+            //};
+
+            Employee emp = new Employee();
+            emp.FirstName = "johnson";
+            emp.LastName = " fernandes";
+            emp.Salary = 14000;
+            employees.Add(emp);
+
+            emp = new Employee();
+            emp.FirstName = "michael";
+            emp.LastName = "jackson";
+            emp.Salary = 16000;
+            employees.Add(emp);
+
+            emp = new Employee();
+            emp.FirstName = "robert";
+            emp.LastName = " pattinson";
+            emp.Salary = 20000;
+            employees.Add(emp);
+
+            return employees;
+        }
+    }
+}
---
+++ WebApplication4/Controllers/TestController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+using WebApplication4.ViewModels;
+
+namespace WebApplication4.Controllers
+{
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+    }
+    public class TestController : Controller
+    {
+        [NonAction]
+        public string GetCustomer()
+        {
+            Customer c = new Customer();
+            c.CustomerName = "Customer 1";
+            c.Address = "Address 1";
+            return c.CustomerName + "|" + c.Address;
+        }
+        public ActionResult GetView()
+        {
+            /*
+            Employee emp = new Employee();
+            emp.FirstName = "Sukesh";
+            emp.LastName = "Marla";
+            emp.Salary = 20000;
+
+            EmployeeViewModel vmEmp = new EmployeeViewModel();
+            vmEmp.EmployeeName = emp.FirstName + " " + emp.LastName;
+            vmEmp.Salary = emp.Salary.ToString("C");
+            if (emp.Salary > 15000)
+            {
+                vmEmp.SalaryColor = "yellow";
+            }
+            else
+            {
+                vmEmp.SalaryColor = "green";
+            }
+            
+            //vmEmp.UserName = "Admin";
+            return View("myview", vmEmp);
+            //ViewData["Employee"] = emp;
+            //ViewBag.Employee = emp;
+            //return View("MyView",emp);
+            */
+
+            EmployeeListViewModel employeeListViewModel = new EmployeeListViewModel();
+            EmployeeBusinessLayer empBal = new EmployeeBusinessLayer();
+            List<Employee> employees = empBal.GetEmployees();
+            List<EmployeeViewModel> empViewModels = new List<EmployeeViewModel>();
+
+            foreach (Employee emp in employees)
+            {
+                EmployeeViewModel empViewModel = new EmployeeViewModel();
+                empViewModel.EmployeeName = emp.FirstName + " " + emp.LastName;
+                empViewModel.Salary = emp.Salary.ToString("C");
+                if (emp.Salary > 15000)
+                {
+                    empViewModel.SalaryColor = "yellow";
+                }
+                else
+                {
+                    empViewModel.SalaryColor = "green";
+                }
+                empViewModels.Add(empViewModel);
+            }
+            employeeListViewModel.Employees = empViewModels;
+            employeeListViewModel.UserName = "Admin";
+            return View("MyView", employeeListViewModel);
+        }
+
+    }
+}
